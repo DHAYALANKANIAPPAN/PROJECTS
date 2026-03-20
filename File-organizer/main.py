ee7abb26cf0de9abac90fa9c45878e88016e29d2
Dhayalan <dhaya3486@gmail.com>
2026-03-20 20:56:01 +0530

    Update main.py

--- File-organizer/main.py
+++ File-organizer/main.py
@@ -1,1 +1,59 @@
 
+from pathlib import Path
+
+testfolder = Path(r'C:\Users\DHAYA\Desktop\New folder')
+
+mapping = {
+    "Images": [".jpg", ".png"],
+    "Documents": [".pdf", ".docx", ".txt"],
+    "Videos": [".mp4", ".mkv"],
+    "Security": [".log", ".pcap", ".exe"]
+}
+def move_file(file, subfolder):
+    destination = subfolder / file.name
+    counter = 1
+
+    while destination.exists():
+        destination = subfolder / f"{file.stem}_{counter}{file.suffix}"
+        counter += 1
+
+    file.rename(destination)
+
+def safe_move(file, subfolder):
+    try:
+        move_file(file, subfolder)
+        return True
+    except Exception as e:
+        print(f"Error moving {file.name}: {e}")
+        return False
+
+for file in testfolder.iterdir():
+    if file.is_file():
+        ext = file.suffix.lower()
+        moved = False
+
+        if ext == "":
+           subfolder = testfolder / "NoExtension"
+           subfolder.mkdir(exist_ok=True)
+
+           if safe_move(file, subfolder):
+            print(f"Moved {file.name} → NoExtension/")
+           moved = True
+           continue
+
+        for folder, extensions in mapping.items():
+            if ext in extensions:
+                subfolder = testfolder / folder
+                subfolder.mkdir(exist_ok=True)
+
+                if safe_move(file, subfolder):
+                 print(f"Moved {file.name} → {folder}/")
+                moved = True
+                break
+
+        if not moved:
+            subfolder = testfolder / "Others"
+            subfolder.mkdir(exist_ok=True)
+
+            if safe_move(file, subfolder):
+             print(f"Moved {file.name} → Others/")
